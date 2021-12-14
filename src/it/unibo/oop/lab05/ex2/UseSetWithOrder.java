dbeb89fa9b2ea28dc220d1b613e3588f45178995
Fabio Veroli <fabio.veroli@studio.unibo.it>
2021-12-14 13:58:00 +0100

    Class implemented

--- src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
+++ src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
@@ -8,29 +8,32 @@ import java.util.TreeSet;
 
 public final class UseSetWithOrder {
 
-    private UseSetWithOrder() {
-    }
-    public static final int ELEM = 100;
-    /**
-     * @param args
-     *            ignored
-     */
-    public static void main(final String[] args) {
-        /*
-         * Write a program which:
-         * 
-         * 1) Creates a new ORDERED TreeSet of Strings. To order the set, define
-         * a new Comparator in a separate class.
-         */
-    	final var set = new TreeSet<>(new MyStringComparator());
-         /* 2) Inserts in the set a hundred strings containing randomly generated
-         * numbers (use Math.random())
-         */
-    	for(int i = 0; i < ELEM; i++) {
-    		set.add(Double.toString(Math.random()));
-    	}
-         /* 3) Prints the set, which must be ordered
-         */
-    	System.out.print(set);
-    }
+	private UseSetWithOrder() {
+	}
+
+	public static final int ELEM = 100;
+
+	/**
+	 * @param args ignored
+	 */
+	public static void main(final String[] args) {
+		/*
+		 * Write a program which:
+		 * 
+		 * 1) Creates a new ORDERED TreeSet of Strings. To order the set, define a new
+		 * Comparator in a separate class.
+		 */
+		final var set = new TreeSet<>(new MyStringComparator());
+		/*
+		 * 2) Inserts in the set a hundred strings containing randomly generated numbers
+		 * (use Math.random())
+		 */
+		for (int i = 0; i < ELEM; i++) {
+			set.add(Double.toString(Math.random()));
+		}
+		/*
+		 * 3) Prints the set, which must be ordered
+		 */
+		System.out.print(set);
+	}
 }
