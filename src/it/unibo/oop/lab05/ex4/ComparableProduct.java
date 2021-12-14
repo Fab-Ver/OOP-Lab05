ba0055e285ed7e1964fc8b99a23713f2c32857f5
Fabio Veroli <fabio.veroli@studio.unibo.it>
2021-12-14 14:14:05 +0100

    Implemeted a new type of Product that implements Comparable

--- src/it/unibo/oop/lab05/ex4/ComparableProduct.java
+++ src/it/unibo/oop/lab05/ex4/ComparableProduct.java
@@ -5,12 +5,18 @@ import it.unibo.oop.lab05.ex3.ProductImpl;
 
 public class ComparableProduct extends ProductImpl implements Comparable<Product> {
 
+	/**
+	 * 
+	 * @param name     name of the comparable product.
+	 * @param quantity quantity of the comparable product.
+	 */
 	public ComparableProduct(String name, double quantity) {
 		super(name, quantity);
 	}
-	
+
+	@Override
 	public int compareTo(final Product o) {
-        return getName().compareTo(o.getName());
-    }
+		return getName().compareTo(o.getName());
+	}
 
 }
