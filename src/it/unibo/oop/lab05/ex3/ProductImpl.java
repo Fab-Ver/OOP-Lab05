92b4b1a5ee1e86bd478609b46c2b45c202fc2790
Fabio Veroli <fabio.veroli@studio.unibo.it>
2021-12-14 14:04:56 +0100

    Implemented interface Product

--- src/it/unibo/oop/lab05/ex3/ProductImpl.java
+++ src/it/unibo/oop/lab05/ex3/ProductImpl.java
@@ -1,32 +1,33 @@
 package it.unibo.oop.lab05.ex3;
 
-
-
 public class ProductImpl implements Product {
-	
-    private final String name;
+
+	private final String name;
 	private final double quantity;
-	
+
+	/**
+	 * 
+	 * @param name     name of the product
+	 * @param quantity quantity of the product
+	 */
 	public ProductImpl(String name, double quantity) {
 		super();
 		this.name = name;
 		this.quantity = quantity;
 	}
 
+	@Override
 	public String getName() {
 		return this.name;
 	}
 
+	@Override
 	public double getQuantity() {
 		return this.quantity;
 	}
-
+	@Override
 	public String toString() {
 		return "ProductImpl [name=" + this.name + ", quantity=" + this.quantity + "]";
 	}
-
-	
-	
-	
 
 }
