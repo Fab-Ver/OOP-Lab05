bcaeeec784b197c58960c45a9f0d03e784b7e93a
Fabio Veroli <fabio.veroli@studio.unibo.it>
2021-12-14 13:56:57 +0100

    Defined a new String comparator

--- src/it/unibo/oop/lab05/ex2/MyStringComparator.java
+++ src/it/unibo/oop/lab05/ex2/MyStringComparator.java
@@ -3,6 +3,11 @@ package it.unibo.oop.lab05.ex2;
 import java.util.Comparator;
 
 public class MyStringComparator implements Comparator<String> {
+	/**
+	 * @param s1 first string to compare
+	 * 
+	 * @param s2 second string to compare  
+	 */
 	public int compare(final String s1, final String s2) {
 		return Double.compare(Double.parseDouble(s1), Double.parseDouble(s2));
 	}
