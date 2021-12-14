5c7fe844133cebfb430fd2d9c0ec211a0087beca
Fabio Veroli <fabio.veroli@studio.unibo.it>
2021-12-14 14:05:25 +0100

    Implemented interface Warehouse

--- src/it/unibo/oop/lab05/ex3/WarehouseImpl.java
+++ src/it/unibo/oop/lab05/ex3/WarehouseImpl.java
@@ -5,45 +5,49 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class WarehouseImpl implements Warehouse {
-	
+
 	private final Set<Product> set = newSet();
-	
-	
+
+	@Override
 	public void addProduct(Product p) {
 		this.set.add(p);
 	}
 
+	@Override
 	public Set<String> allNames() {
 		final Set<String> s = newSet();
-		for(Product p : this.set) {
-			 s.add(p.getName());
+		for (Product p : this.set) {
+			s.add(p.getName());
 		}
 		return s;
 	}
 
+	@Override
 	public Set<Product> allProducts() {
 		return newSetFrom(this.set);
 	}
 
+	@Override
 	public boolean containsProduct(Product p) {
 		return set.contains(p);
 	}
 
+	@Override
 	public double getQuantity(String name) {
-		for(Product p : this.set) {
-			if(p.getName().equals(name)) {
+		for (Product p : this.set) {
+			if (p.getName().equals(name)) {
 				return p.getQuantity();
 			}
 		}
 		return -1;
 	}
-	
+
 	private static <E> Set<E> newSet() {
-        return new LinkedHashSet<>();
-    }
-	
+		return new LinkedHashSet<>();
+	}
+
 	private static <E> Set<E> newSetFrom(final Collection<E> origin) {
-        return new LinkedHashSet<>(origin);
-    }
+		return new LinkedHashSet<>(origin);
+	}
 
 }
